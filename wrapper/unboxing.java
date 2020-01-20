6e2e06d99cf3b69da22dde87d331c81db92a057f
BadushaNazar <58019605+BadushaNazar@users.noreply.github.com>
2020-01-20 18:26:03 +0530

    Add files via upload

---
+++ wrapper/unboxing.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		Integer a=new Integer(10);
+		int i=a.intValue();
+		int j=i;
+		System.out.println(a+" "+i+" "+j);
+		
+	}
+
+}
